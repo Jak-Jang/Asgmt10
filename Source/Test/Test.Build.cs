1473299fd188924d20a8e1ad5cba45abb8dcd648
Jak-Jang <jjgk24211@gmail.com>
2025-09-04 17:12:36 +0900

    feat(Module, Plugin)
    - Module(Test) 추가
    - Plugin(Temporary) 추가

---
+++ Source/Test/Test.Build.cs
@@ -0,0 +1,24 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Test : ModuleRules
+{
+	public Test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+		});
+	}
+}
